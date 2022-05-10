a2515ee8a6404413b8bd6356eba63087cbc3ea14
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-10 05:22:54 +0000

    [update]4-18_エラー備忘録

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
   devise_for :users
-  get 'homes/about' => "homes#about", as: "about"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only:[:show, :edit, :update]
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    # post_commnets_controllerの名前ミスしている為、ルーティングもコントローラ名に合わせて変更
+    resources :post_commnets, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
